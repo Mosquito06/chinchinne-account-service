b85cbb44483f321d4058d5b9549c6cefd8216b8b
chinchin <skykim10908@naver.com>
2023-03-30 11:50:17 +0900

    add mapping info and update modelmapper ver

--- src/main/java/com/chinchinne/accountservice/AccountServiceApplication.java
+++ src/main/java/com/chinchinne/accountservice/AccountServiceApplication.java
@@ -1,9 +1,14 @@
 package com.chinchinne.accountservice;
 
+import com.chinchinne.accountservice.domain.entity.Account;
+import com.chinchinne.accountservice.domain.model.Status;
+import com.chinchinne.accountservice.model.AccountDto;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.math.BigInteger;
 
 @SpringBootApplication
 public class AccountServiceApplication
@@ -15,6 +20,21 @@ public class AccountServiceApplication
     @Bean
     public ModelMapper modelMapper()
     {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.typeMap(Account.class, AccountDto.class).addMappings( mapping ->
+        {
+            mapping.map(Account::getAccountId, AccountDto::setAccountId);
+            mapping.map( row -> row.getUserId().getId(), AccountDto::setUserId);
+            mapping.map( row -> row.getAccountDate().getYear()  , AccountDto::setYear);
+            mapping.map( row -> row.getAccountDate().getMonth()  , AccountDto::setMonth);
+            mapping.map( row -> row.getAccountDate().getDay()  , AccountDto::setDay);
+            mapping.map( row -> String.valueOf(row.getCategoryId().getId()) , AccountDto::setCategory);
+            mapping.map(Account::getStatus, AccountDto::setStatus);
+            mapping.map(Account::getMemo, AccountDto::setMemo);
+            mapping.map(Account::getAmount, AccountDto::setAmount);
+        });
+
+        return modelMapper;
     }
 }
